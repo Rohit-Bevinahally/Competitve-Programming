33d826d46c1331a40ceeabe8eb57c2e296c91187
Rohit-Bevinahally <74117976+Rohit-Bevinahally@users.noreply.github.com>
2023-08-22 02:35:06 +0530

    Initial Commit

---
+++ Template.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{
+// Data type definitions
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+
+// Macros
+#define loop(i,k,n) for(auto i=k; i < n; i++)
+#define rloop(i,n,k) for(auto i=n-1; i >= k; i--)
+#define trv(x,a) for(auto& x:a)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define ins insert
+#define sz(x) (int)x.size()
+#define f first
+#define s second
+
+// Min-Max : 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+#define armin(v) *min_element(all(v))
+#define armax(v) *max_element(all(v))
+
+// I/O :
+template<typename T1, typename T2>
+istream& operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); } 
+template<typename T1, typename T2>
+ostream& operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
+template<typename T>
+istream& operator>>(istream &istream, vector<T> &x){ trv(a,x) istream >> a; return istream; }
+template<typename T>
+ostream& operator<<(ostream &ostream, const vector<T> &x) { trv(a,x) ostream << a << " "; return ostream; }
+
+template<class T> void inp(T& x) { cin >> x; }
+template<class H, class... T> void inp(H& h, T&... t) { inp(h); inp(t...); }
+template<class A> void inp(vector<vector<A>>& x) { trv(a,x) inp(a); }
+
+// Debug :
+#ifdef LOCAL
+#include "../Library/debug.cpp"
+#else
+#define debug(...) ""
+#endif
+
+// rng :
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+// }}}
+
+// Constants :
+const int mod = 1e9 + 7; //998244353;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ld eps = 1e-9;
+const char nl = '\n';
+
+void solve() {
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    int tc = 1;
+    cin >> tc;     
+    while(tc--) {
+        solve();
+    }
+ 	return (0-0);
+}
+
